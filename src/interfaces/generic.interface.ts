d69c6d0b7b171bee916305aa4afd033388893a83
0xjuju <jermol.jupiter@gmail.com>
2024-09-19 01:28:46 -0400

    add JSDoc to FileTree interfacec

--- src/interfaces/generic.interface.ts
+++ src/interfaces/generic.interface.ts
@@ -1,4 +1,7 @@
-
+/**
+ * Interface representing nested dictionary
+ * @property key - field can be either a string or a FileTree Object
+ */
 interface FileTree {
     [key: string]: FileTree | string;
 }
